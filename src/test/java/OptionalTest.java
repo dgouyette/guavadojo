8b19dc32feb55ce8abf16f7ddfd540540802b369
Damien GOUYETTE <damien.gouyette@gmail.com>
2012-12-15 15:48:40 +0100

    add optional test

---
+++ src/test/java/OptionalTest.java
@@ -0,0 +1,32 @@
+import com.google.common.base.Optional;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class OptionalTest {
+
+
+    public Optional<Integer> stringToInt(String intValue) {
+        if (StringUtils.isNumeric(intValue)) {
+            return Optional.of(Integer.valueOf(intValue));
+        } else {
+            return Optional.absent();
+        }
+    }
+
+
+    @Test
+    public void should_return_none_when_non_numeric_value() {
+        assertThat(stringToInt("AAA").isPresent()).isEqualTo(false);
+    }
+
+
+    @Test
+    public void should_return_Integer_value_when_numeric_value() {
+        assertThat(stringToInt("123").isPresent()).isEqualTo(true);
+        assertThat(stringToInt("123").get()).isEqualTo(123);
+    }
+
+
+}
