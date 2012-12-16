759c1e9a9372e30fd91e25379a3cda87c3521c76
Thomas Queste <tqueste@gmail.com>
2012-12-16 14:37:57 +0100

    use Fest isTrue/isFalse asserts

--- src/test/java/guavadojo/OptionalTest.java
+++ src/test/java/guavadojo/OptionalTest.java
@@ -1,10 +1,11 @@
 package guavadojo;
 
-import com.google.common.base.Optional;
+import static org.fest.assertions.Assertions.assertThat;
+
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
-import static org.fest.assertions.Assertions.assertThat;
+import com.google.common.base.Optional;
 
 public class OptionalTest {
   
@@ -18,12 +19,12 @@ public class OptionalTest {
   
   @Test
   public void should_return_none_when_non_numeric_value() {
-    assertThat(stringToInt("AAA").isPresent()).isEqualTo(false);
+    assertThat(stringToInt("AAA").isPresent()).isFalse();
   }
   
   @Test
   public void should_return_Integer_value_when_numeric_value() {
-    assertThat(stringToInt("123").isPresent()).isEqualTo(true);
+    assertThat(stringToInt("123").isPresent()).isTrue();
     assertThat(stringToInt("123").get()).isEqualTo(123);
   }
   
